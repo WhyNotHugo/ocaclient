c984dc418557d8cf7c4aa0888095c20648531404
Hugo Osvaldo Barrera <hugo@barrera.io>
2022-03-15 19:11:08 +0100

    Drop support for Python 3.6

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
         "Operating System :: POSIX",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
